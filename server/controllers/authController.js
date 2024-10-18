9338349773f7bb7437bfd542b92697108cbae3c6
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-10-18 20:04:42 +0500

    Add bcryptjs dependency and implement user sign up functionality in authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,16 @@
-const signUp = async (req, res) => {};
+const bcryptjs = require("bcryptjs");
+const User = require("../models/userModel");
+const signUp = async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const newUser = new User({ username, email, password: hashedPassword });
+    await newUser.save();
+    res.status(201).json("User created successfully!");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 const signIn = async (req, res) => {};
 const signOut = async (req, res) => {};
 
