5522aa8a81749c040fcb3e8ef914c3a4a80f3e63
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-10-18 20:04:42 +0500

    Implement user signUp signIn signOut functionalities in authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,58 @@
-const signUp = async (req, res) => {};
-const signIn = async (req, res) => {};
-const signOut = async (req, res) => {};
+const bcryptjs = require("bcryptjs");
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+const errorHandler = require("../utils/error");
+
+const signUp = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  try {
+    if (!username || !email || !password) {
+      return next(errorHandler(400, "All fields are required"));
+    }
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json("User already registered!");
+    }
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const newUser = new User({ username, email, password: hashedPassword });
+    await newUser.save();
+    res.status(201).json("User created successfully!");
+  } catch (error) {
+    next(errorHandler(500, error.message));
+  }
+};
+const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) {
+      return res.status(400).json("All fields are required");
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json("User not found");
+    }
+    const isMatch = bcryptjs.compareSync(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json("Invalid credentials");
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = user._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(errorHandler(500, error.message));
+  }
+};
+const signOut = async (req, res, next) => {
+  try {
+    res.clearCookie("access_token");
+    res.status(200).json("User has been logged out!");
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = {
   signUp,
--- server/server.js
+++ server/server.js
@@ -20,6 +20,16 @@ app.get("/*", (req, res) => {
 
 app.use("/api/auth", authRouter);
 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
 app.listen(port, () => {
   connectDB();
   console.log(`Server is running on http://localhost:${port}`);
