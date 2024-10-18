91d2d966af3ce28ae62adb5f31430f4b66372322
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-10-18 20:27:57 +0500

    modify with comments for easy understand

--- server/server.js
+++ server/server.js
@@ -8,18 +8,22 @@ app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
+// Home Route
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+// 404 Route
 app.get("/*", (req, res) => {
   res.status(404).json({
     message: "Route not found",
   });
 });
 
+// Routes
 app.use("/api/auth", authRouter);
 
+// Error handler
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
@@ -30,6 +34,7 @@ app.use((err, req, res, next) => {
   });
 });
 
+// Server
 app.listen(port, () => {
   connectDB();
   console.log(`Server is running on http://localhost:${port}`);
