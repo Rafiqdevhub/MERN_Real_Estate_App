4d5626e61e3aeb67d1daa560113b4d7a049df637
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-10-18 08:39:05 +0500

    create a Header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,60 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
+// import { useSelector } from "react-redux";
+
 const Header = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  //   const { currentUser } = useSelector();
+  const handleSubmit = () => {};
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header className="bg-slate-700 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-500">PropertyPro</span>
+          </h1>
+        </Link>
+        <form
+          onSubmit={handleSubmit}
+          className="bg-slate-600 p-3 rounded-lg flex items-center"
+        >
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button>
+            <FaSearch className="text-slate-400" />
+          </button>
+        </form>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-400 hover:underline">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-400 hover:underline">
+              About
+            </li>
+          </Link>
+          {/* <Link to="/profile">
+            {currentUser ? (
+              <img
+                className="rounded-full h-7 w-7 object-cover"
+                src={currentUser.avatar}
+                alt="profile"
+              />
+            ) : (
+              <li className=" text-slate-700 hover:underline"> Sign in</li>
+            )}
+          </Link> */}
+        </ul>
+      </div>
+    </header>
   );
 };
 
