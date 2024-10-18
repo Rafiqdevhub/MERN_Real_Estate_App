7b4466b19a9276ae3767b2233e2291085d993dea
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-10-18 09:12:38 +0500

    auth controller added to the app

---
+++ server/controllers/authController.js
@@ -0,0 +1,9 @@
+const signUp = async (req, res) => {};
+const signIn = async (req, res) => {};
+const signOut = async (req, res) => {};
+
+module.exports = {
+  signUp,
+  signIn,
+  signOut,
+};
--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 require("dotenv").config();
 const connectDB = require("./config/dbConnection");
+const authRouter = require("./routes/authRoute");
 
 const app = express();
+app.use(express.json());
+
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
@@ -14,6 +17,8 @@ app.get("/*", (req, res) => {
     message: "Route not found",
   });
 });
+
+app.use("/api/auth", authRouter);
 
 app.listen(port, () => {
   connectDB();
