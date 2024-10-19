ba363e62ef6d97a5482624a691412590b962ea01
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-10-19 10:54:08 +0500

    try to fixe the app

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,6 +5,7 @@ const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASEURL,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MEESSAGINSENDERID,
   appId: import.meta.env.VITE_FIREBASE_APPID,
