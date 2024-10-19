5571d65987d39671c2fc6a14f442ac4ff8efc8c5
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-10-19 18:57:59 +0500

    Refactor firebase.js to include storage initialization

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -10,7 +10,5 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
-
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
+export const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
