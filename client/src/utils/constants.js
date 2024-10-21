9a49757443ef1d2981840a489441e441b1f71fce
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-10-21 18:39:11 +0500

    change the port number of the app

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,2 +1,2 @@
-export const AUTH_API_END_POINT = "http://localhost:3000/api/auth";
-export const USER_API_END_POINT = "http://localhost:3000/api/user";
+export const AUTH_API_END_POINT = "http://localhost:5000/api/auth";
+export const USER_API_END_POINT = "http://localhost:5000/api/user";
