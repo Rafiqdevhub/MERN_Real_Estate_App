6a3b4d7bcf28180282485b0b01602d06c7bbe691
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-10-21 18:44:31 +0500

    create  listing added the ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Search from "./pages/Search";
 import Header from "./components/Header";
 import Profile from "./pages/Profile";
 import PrivateRoute from "./components/PrivateRoute";
+import CreateListing from "./pages/CreateListing";
 const App = () => {
   return (
     <BrowserRouter>
@@ -19,6 +20,7 @@ const App = () => {
         <Route path="/search" element={<Search />} />
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
+          <Route path="/create-listing" element={<CreateListing />} />
         </Route>
       </Routes>
     </BrowserRouter>
