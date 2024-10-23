a40bd57f6b953c2abc3e865dbdde272b453ad345
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-10-23 18:15:38 +0500

    change the urls ports

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,2 +1,3 @@
 export const AUTH_API_END_POINT = "http://localhost:5000/api/auth";
 export const USER_API_END_POINT = "http://localhost:5000/api/user";
+export const LISTING_API_END_POINT = "http://localhost:5000/api/listing";
