2898a9537a5a1a9f12856af901153f06d323afe3
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-10-25 19:54:02 +0500

    Refactor Header component to include search functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,27 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { FaSearch } from "react-icons/fa";
 import { useSelector } from "react-redux";
 
 const Header = () => {
-  const [searchTerm, setSearchTerm] = useState("");
   const { currentUser } = useSelector((state) => state.user);
-  const handleSubmit = () => {};
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
   return (
     <header className="bg-slate-900 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -21,7 +36,7 @@ const Header = () => {
         >
           <input
             type="text"
-            placeholder="Search"
+            placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
