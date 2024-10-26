9c0df7de3b08e40ff67c7534d3976d145406d7b0
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-10-26 11:05:10 +0500

    Refactor currency formatting in Listing component

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -94,8 +94,8 @@ const Listing = () => {
             <p className="text-2xl font-semibold">
               {listing.name} - ${" "}
               {listing.offer
-                ? listing.discountPrice.toLocaleString("en-US")
-                : listing.regularPrice.toLocaleString("en-US")}
+                ? listing.discountPrice.toLocaleString("en-PKR")
+                : listing.regularPrice.toLocaleString("en-PKR")}
               {listing.type === "rent" && " / month"}
             </p>
             <p className="flex items-center mt-6 gap-2 text-slate-600  text-sm">
